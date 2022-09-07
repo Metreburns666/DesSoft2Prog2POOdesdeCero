e749187c5e9863d28b8c27fd644cf86d8f8117b3
Lucas Metrebian <lucas.metrebian@gmail.com>
2022-09-06 21:13:02 -0300

    Se modifica ejercicio 13

--- ejemploClaseyObj13.py
+++ ejemploClaseyObj13.py
@@ -14,3 +14,22 @@
 #este metodo puede ser llamado sin crear
 #un instancia de la clase
 #Es decir no se utiliza __init__
+
+class Pastel:
+    def __init__(self,ingredientes):
+        self.ingredientes=ingredientes
+    
+    def __repr__(self):
+        return f"pastel({self.ingredientes !r}"
+    @classmethod
+
+    def Pastel_chocolate(cls):
+        return cls(["Harina","Leche","Chocolate"])
+
+    @classmethod
+
+    def Pastel_vainilla(cls):
+        return cls(["Harina","Leche","Vainilla"])
+
+print(Pastel.Pastel_chocolate())
+print(Pastel.Pastel_vainilla())
