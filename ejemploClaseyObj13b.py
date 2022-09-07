c4e52644412e82cffacb410671d19b24fdf62106
Lucas Metrebian <lucas.metrebian@gmail.com>
2022-09-06 21:40:43 -0300

    Se modifica Ejemplo 13b

--- ejemploClaseyObj13b.py
+++ ejemploClaseyObj13b.py
@@ -14,4 +14,22 @@
 #*Pueden ser llamados sin tener una instancia
 #*de la clase, ademas este tipo de metodos
 #*no tienen acceso al exterior
+#*Por lo cual no pueden acceder a ningun otro
+#*Atributo o llamar a ninguna otra funcion dentro
+#*de la clase
+import math
+class Pastel:
+    def __init__(self,ingredientes,tamaño):
+        self.ingredientes = ingredientes
+        self.tamaño = tamaño
+    def __repr__(self):
+        return(f"Pastel({self.ingredientes},"f"{self.tamaño})")
+    def area(self):
+        return self.tamaño_area(self.tamaño)
+    @staticmethod
+    def tamaño_area(A):
+        return A ** 2 *math.pi  # los 2 ** significa igual igual es lo mismo que ==
 
+nuevo_pastel= Pastel(["Harina","Huevo","Azucar","Leche","Limon"],4)
+print(nuevo_pastel.ingredientes)
+print(nuevo_pastel.tamaño_area(12))
