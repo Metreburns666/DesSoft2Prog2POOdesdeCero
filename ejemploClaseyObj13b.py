7e6e76bb38729b88cf2b628fcc60ef59752a5a62
Lucas Metrebian <lucas.metrebian@gmail.com>
2022-09-06 21:16:34 -0300

    Se crea Ejemplo 13b

---
+++ ejemploClaseyObj13b.py
@@ -0,0 +1,12 @@
+#metodos Clase y Estaticos
+#Instancia: Un objeto se crea usando el constructor
+# de una clase
+#Una vez que el objeto es creado
+#se lo conoce como una instancia de la clase
+
+#Tipos de metodos:
+#*Metodos Estaticos
+#*Metodos Clase
+#*Metodos Instancia
+
+#*Metodo Estatico
